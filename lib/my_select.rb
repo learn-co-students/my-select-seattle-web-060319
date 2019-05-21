c78faeb155c3168294efd9a81d46ba770b266698
Kara O <karajo.odle@gmail.com>
2019-05-21 21:49:37 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
- # your code here!
+  i = 0 
+  new_array = []
+
+  while i < collection.length do
+    
+    if yield(collection[i]) == true
+      new_array << collection[i]
+    end
+    
+    i = i + 1 
+    
+  end
+  
+  
+ new_array
+ 
 end
