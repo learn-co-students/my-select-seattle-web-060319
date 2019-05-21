dbad43780652ec09ad474951885c0fb0a3f3a284
Danie Martinoli <daniechris5@gmail.com>
2019-05-21 13:50:07 -0700

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,10 @@ def my_select(array)
   collection = []
   i = 0
   while i < array.length
-    collection.find_all i % 2 == 0 yield(array[i])
-    i = i + 1
+     if yield(array[i])
+       collection << array[i]
+     end
+     i = i + 1
   end
-    collection.select
+  collection
 end
