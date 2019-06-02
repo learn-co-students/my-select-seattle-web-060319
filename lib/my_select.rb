5a68ae806c24c48d16a3f5286e68f1054b31362a
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 07:29:19 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  i = 0
+  j = 0
+  newarray = []
+  while i < array.length
+    if(yield(array[i]))
+      newarray[j] = array[i]
+      j = j + 1
+    end
+      i = i + 1
+    end
+    return newarray
 end
